3f966d481a86d9ecb9db0f6e3f92bbbdb6d92432
Jay <jaypatel0523@gmail.com>
2023-07-11 15:36:36 +0530

    cors changes

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors({
-    origin: '*',
+    origin: ["http://localhost:8000"],
     credentials: true
 }));
 
